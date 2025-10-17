9ad0945166d742fe2fe4d848c0a82d09475c0687
Rafaelly Diniz <rafaellypoliveira@gmail.com>
2025-10-17 20:58:01 -0300

    feat: add bonus folder and enhance fractal features
    - Created `bonus/` folder inside `src/` to store bonus part source files.
    - Added keyboard hooks to move the fractal.
    - Implemented dynamic zoom based on mouse position.
    - Added color schemes controlled via hook.
    - Introduced Burning Ship and Tricorn fractals.
    - Optimized code using enums and switch statements.

---
+++ src/bonus/fractal_colors_bonus.c
@@ -0,0 +1,45 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   fractal_colors_bonus.c                             :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: marvin <marvin@student.42.fr>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/10/17 19:46:36 by marvin            #+#    #+#             */
+/*   Updated: 2025/10/17 19:46:36 by marvin           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "fractol.h"
+
+void	get_color(t_fractal *fractal)
+{
+	switch (fractal->color_scheme)
+	{
+	case SCHEME_1:
+		fractal->color_start = MANDEL_BLUE;
+		fractal->color_end = MANDEL_GOLD;
+		break;
+	case SCHEME_2:
+		fractal->color_start = JULIA_PURPLE;
+		fractal->color_end = JULIA_CIAN;
+		break;
+	case SCHEME_3:
+		fractal->color_start = SHIP_BLUE;
+		fractal->color_end = SHIP_ORANGE;
+		break;
+	case SCHEME_4:
+		fractal->color_start = SAF_RED;
+		fractal->color_end = SAF_PINK;
+		break;
+	}
+}
+
+void	put_color(t_fractal *fractal, int i, int x, int y)
+{
+	int	color;
+
+	get_color(fractal);
+	color = map(i, fractal->color_start, fractal->color_end, fractal->definition);
+	put_pixel(fractal->img, x, y, color);
+}
